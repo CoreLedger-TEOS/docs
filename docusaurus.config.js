e679cab6fe1048e43b359613459a454f818215c2
Andriy Lisovoy <Andriy.Lisovoy@coreledger.net>
2026-06-23 17:02:56 +0200

    Fix Docusaurus base URL for Azure prefix

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -10,7 +10,7 @@ const config = {
   favicon: 'img/favicon.ico',
 
   url: 'https://docs.coreledger.net',
-  baseUrl: '/',
+  baseUrl: '/docusaurus/',
   trailingSlash: false,
 
   organizationName: 'CoreLedger',
